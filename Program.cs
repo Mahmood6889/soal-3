5105679ff3181007ed32f02be0342ee3c053e7e0
mahmood6889 <mahmood_sh6889@yahoo.com>
2022-03-11 14:46:38 +0330

    soal3

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿int a;
+for (var i = 0; i < 3 ; i++)
+{
+    System.Console.WriteLine("hoghoh ro bede");
+    a=Convert.ToInt32(Console.ReadLine());
+    if (a>2000000 && a<3000000)
+    {
+        System.Console.WriteLine(a*0.05);
+        
+    }else if (a>=3000000)
+    {
+            System.Console.WriteLine(a*0.07);
+    }
+    else
+    {
+                System.Console.WriteLine("maliat=0");
+    }
+}
